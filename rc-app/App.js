793297ff0746dd3112378fa0e98fe07f15399653
Kicho115 <oscar.leonardo.ang@gmail.com>
2024-10-03 02:11:27 -0700

    Add the Bluetooth connection modal layout

--- rc-app/App.js
+++ rc-app/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable, Modal, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -15,11 +15,35 @@ const ConnectButton = (props) => {
   );
 }
 
+const BluetoothConnectModal = ({visible, onClose}) => {
+  return (
+    <Modal
+      visible={visible}
+      transparent={true}
+      animationType="slide"
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>List of devices</Text>
+          <Pressable style={styles.modalButton} onPress={onClose}>
+            <Text style={{color: 'white', fontWeight: '600'}}>Close</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>)
+}
+
 const MainScreen = ({navigation}) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   return (
     <View style={styles.mainScreen}>
       <Text style={styles.title}>Brokeneitor</Text>
-      <ConnectButton />
+      <ConnectButton onPress={() => setIsModalVisible(true)} />
+      <BluetoothConnectModal 
+        visible={isModalVisible} 
+        onClose={() => setIsModalVisible(false)}
+      />
       <Button
         title="Go to Controls"
         onPress={() => navigation.navigate('Controls')}
@@ -84,5 +108,32 @@ const styles = StyleSheet.create({
     color: Colors.primary,
     fontWeight: '900',
     fontSize: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: Colors.secondary,
+    borderRadius: 20,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    width: '60%',
+    gap: 20,
+  },
+  modalTitle: {
+    color: 'black',
+    fontWeight: '600',
+    fontSize: 20,
+  },
+  modalButton: {
+    backgroundColor: Colors.background,
+    color: Colors.primary,
+    borderRadius: 10,
+    padding: 9,
   }
 });
